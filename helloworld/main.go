8865c2995989177debdb376dbf2d4efa3fd3b343
Alex Sahm <sahm.alexander@pwc.com>
2020-01-06 13:25:10 +0100

    hello world in golang

---
+++ helloworld/main.go
@@ -0,0 +1,15 @@
+/*
+1. What is a package: project name == workspace
+
+2. Two types of packages:
+ - Executable package which you run like 'go run <package-binary>'
+   (main package must have main function)
+ - Reusable package which you use as dependency or library
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
