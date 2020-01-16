c05f3734f6849b9939a6eec4d6d5fcd9332ae1a7
Alex Sahm <sahm.alexander@pwc.com>
2020-01-16 16:45:31 +0100

    added interface code

---
+++ interfaces/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/*
+When you are type with exactly this function 'getGreeting() string'
+then you are a honorary member of type bot. So you can now call the
+function 'func printGreeting(b bot)'.
+
+In Java you say "xyz implements interfaceXYZ" to describe xyz is an implementor of an interface.
+In Go you just implement exactly the same function for a specific receiver.
+Theres is no need to link together 'bot' and 'englishBot/spanishBot' because interfaces are implicit.
+Implicitly, they are connected with each other.
+*/
+type bot interface {
+	// just specify the types
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
